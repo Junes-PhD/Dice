b41b117870e86fa7590061ca6d6a13cb2bf6b4f7
Junes-PhD <asowa42@gmail.com>
2022-11-29 03:26:00 -0600

    error check

--- buttons.py
+++ buttons.py
@@ -7,7 +7,14 @@ class user_select:
 
     def dice_num():
         num = input("1-6:")
-        return num
+        try:
+            n = int(num)
+            if n >= 1 and n <= 6:
+                return n
+            else:
+                user_select.dice_num()
+        except:
+            user_select.dice_num()
 
     def mode():
         mode = input("Select Mode:")
@@ -16,3 +23,12 @@ class user_select:
     def rolls():
         rolls = input("Number of Rolls:")
         return rolls
+    
+    def agree():
+        response = input("Y/N?:")
+        if response == "Y" or response == "y" or response == "Yes" or response == "yes":
+            return "yes"
+        elif response == "N" or response == "n" or response == "No" or response == "no":
+            return "no"
+        else:
+            user_select.agree()
