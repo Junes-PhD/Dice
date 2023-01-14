273487fb04826b33907b77cfd23feb9b8cf5b781
Junes-PhD <asowa42@gmail.com>
2023-01-13 22:00:59 -0600

    wip

--- init.py
+++ init.py
@@ -3,20 +3,93 @@ from array import array
 import display
 import buttons
 import dice
-
+import player
 
 
 class shooting:
     def __init__(self, attacker, defender) -> None:
         self.attacker = attacker
         self.defender = defender
+        self.attacks = self.attacker["BA"]
+        self.ballistic_skill = self.attacker["BS"]
+        self.dmg = self.attacker["BH"]
+        self.crit_dmg = self.attacker["BC"]
+        self.ap = 0
+        self.save_rolls = self.defender["DF"]
+        self.save = self.defender["Sv"]
+        self.hits = 0
+        self.crits = 0
+        self.saves = 0
+        self.crit_saves = 0
+        self.total_damage = 0
+        self.cover = self.defender["Cover"]
+        
+        self.d6=dice.sides(6)
+        
 
     def __str__(self):
         pass
 
     def report(self):
-        print(self.attacker["Name"])
-        print(self.attacker["Sv"])
+        print("Attacker:", self.attacker["Name"])
+        print("BS:", self.attacker["BS"])
+        print("Attacks:", self.attacker["BA"])
+        print("DMG:", self.attacker["BH"], "/", self.attacker["BC"])
+
+    def auto(self):
+        self.hit_round(self.attacks, self.ballistic_skill)
+        self.wound_round(self.hits, self.crits, self.save_rolls, self.ap, self.cover)
+    
+    def hit_round(self, attacks, bs, trait1="", trait2=""):
+        self.d6.roll(attacks)
+        self.hits, self.crits = self.d6.num_plus_with_crits(bs)
+        
+        print(self.d6.results)
+        print("BS:", bs)
+        print(self.hits, "hits")
+        print(self.crits, "crits")
+        
+        return self.hits, self.crits
+
+    def wound_round(self, hits, crits, save_rolls, AP, cover):
+
+        if cover == 1:
+            save_rolls -= 1
+
+        self.d6.roll(save_rolls)
+        print(self.d6.results)
+
+        self.saves, self.crit_saves = self.d6.num_plus_with_crits(self.save)
+        if cover == 1:
+            self.saves += 1
+        print(self.save, "+ save")
+        print(self.saves, "saves")
+        print(self.crit_saves, "crit saves")
+
+        while self.crit_saves > 0:
+            if crits > 0:
+                self.crit_saves -= 1
+                crits -= 1
+            elif hits > 0:
+                self.crit_saves -= 1
+                hits -= 1
+        while self.saves > 0:
+            if self.saves >= 2 and hits <2 and crits > 0:
+                self.saves -= 2
+                crits -= 1
+            elif self.saves > 0 and hits > 0:
+                self.saves -= 1
+                hits -= 1
+            else:
+                self.saves -= 1
+        print(hits, "hits")
+        print(crits, "crits")
+        total_damage = hits * self.dmg + crits * self.crit_dmg
+        print(total_damage, "wounds")
+        return self.total_damage
+
+    def parse_specials(self):
+
 
 class manual:
     def __init__(self, attacks, skill, wound, save, mods) -> None:
@@ -87,15 +160,21 @@ class manual:
             print("exiting")
             return            
         self.d6.roll(x)
-        
+
 
 
 def main():
-    d6 = dice.sides(6)
-    show = display.display()
     
-    dice_calc = manual(0,0,0,0,"")
-    dice_calc.manual()
+    shot = shooting(player.a1, player.d1)
+    #shot.report()
+    shot.auto()
+
+
+    #d6 = dice.sides(6)
+    #show = display.display()
+    
+    #dice_calc = manual(0,0,0,0,"")
+    #dice_calc.manual()
     
     
     #shoot = manual(10, 3, 4, 3, "")
@@ -106,40 +185,6 @@ def main():
     #test = shooting(p1, d1)
     #test.report()
 
-p1 = {
-    "Name": "Intercessor Squad",
-    "M": 6,
-    "WS": 3,
-    "BS": 3,
-    "S": 4,
-    "T": 4,
-    "W": 2,
-    "A": 2,
-    "Ld": 7,
-    "Sv": 3,
-    "Shooting": "Bolt Riftle",
-    "Melee": "Knife",
-}
-
-d1 = {
-    "Name": "Intercessor Sergeant",
-    "M": 6,
-    "WS": 3,
-    "BS": 3,
-    "S": 4,
-    "T": 4,
-    "W": 2,
-    "A": 3,
-    "Ld": 8,
-    "Sv": 3,
-    "Shooting": "Bolt Riftle",
-    "Melee": "Knife",
-}
-
-guns = {
-    "Plasma": {"Range": 12, "Type": "Pistol", "A":1, "S": 6, "AP": -1, "D": 1},
-    "Bolt Rifle": {"Range":30, "Type": "Rapid Fire", "A":1, "S": 4, "AP": -1, "D": 1},
-}
 
 if __name__ == "__main__":
     main()
